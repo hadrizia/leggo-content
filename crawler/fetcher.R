3c219df6aec3ae8a1cd2f788c40200786ee73702
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2019-02-15 12:47:06 -0300

    Removida instalação de pacotes do fetcher

--- crawler/fetcher.R
+++ crawler/fetcher.R
@@ -1,24 +1,10 @@
 #! /usr/bin/Rscript
 
-suppressWarnings(suppressMessages(source(here::here("crawler/utils/constants.R"))))
-suppressWarnings(suppressMessages(source(here::here("crawler/process_data.R"))))
-
-#' @title Install and load a list of packages
-#' @description Install new packages and load all required libraries
-#' @param list_of_packages List of packages
-#' @examples
-#' install_and_load_packages(c("dplyr", "readr"))
-install_and_load_packages <- function(list_of_packages) {
-  new_packages <- suppressMessages(suppressWarnings(
-    list_of_packages[!(list_of_packages %in% installed.packages()[,.PACK])]))
-  
-  if(length(new_packages) > 0) {
-    suppressMessages(suppressWarnings(install.packages(new_packages)))
-  }
-  
-  suppressWarnings(suppressMessages(lapply(list_of_packages, require)))
-  suppressWarnings(suppressMessages(devtools::install_github(.RCONGRESSO_URL, force = TRUE)))
-  suppressWarnings(suppressMessages(devtools::install_github(.LEGGO_URL, force = TRUE)))
+#' @title Load packages
+#' @description Load packages from list
+load_packages <- function(list_of_packages) {
+  print("Loading packages..")
+  suppressWarnings(suppressMessages(lapply(list_of_packages, require, character.only=TRUE)))
 }
 
 #' @title Get arguments from command line option parsing
@@ -47,10 +33,21 @@ get_args <- function() {
   return(opt);
 }
 
-install_and_load_packages(.LIST_OF_PACKAGES)
+list_of_packages <- c("RCurl", "dplyr", "lubridate", "magrittr", "stats", 
+                       "stringr", "tibble", "tidyr", "utils", "devtools", 
+                      "optparse", "here", "rcongresso", "agoradigital")
+
+load_packages(list_of_packages)
+
+suppressWarnings(suppressMessages(source(here::here("crawler/utils/constants.R"))))
+suppressWarnings(suppressMessages(source(here::here("crawler/process_data.R"))))
 
 args <- get_args()
 
+print("Fetching data...")
 df <- fetch_data(args$input)
 
+print("Saving results...")
 save_data(df, args$output)
+
+print("Successfully saved! :D")
